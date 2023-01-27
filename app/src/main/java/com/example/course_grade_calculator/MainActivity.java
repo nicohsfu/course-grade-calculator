5d101138e0aa96ee09454a515b96a9b2cb476237
Nico Hernandez <47044633+nicohsfu@users.noreply.github.com>
2023-01-27 11:54:01 -0800

    initialize assignment logic - displays toast message to notify user that their input is being detected

--- app/src/main/java/com/example/course_grade_calculator/MainActivity.java
+++ app/src/main/java/com/example/course_grade_calculator/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.course_grade_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
+
+    EditText assignmentsEditText;
+
+    double assignmentsGrade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        assignmentsEditText = findViewById(R.id.assignmentsEditText);
+        assignmentsEditText.addTextChangedListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        if (assignmentsEditText.isFocused()) {
+            try {
+                assignmentsGrade = Double.parseDouble(assignmentsEditText.getText().toString());
+            } catch (NumberFormatException e) {
+                assignmentsGrade = 0.0;
+            }
+            Toast.makeText(this, String.valueOf(assignmentsGrade), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
     }
 }
