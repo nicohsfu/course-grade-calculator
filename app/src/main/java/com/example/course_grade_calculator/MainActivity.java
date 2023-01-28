20c25bd06fc2c24867bfcf1165d6a31b4345a828
Nico Hernandez <marcnicohernandez@gmail.com>
2023-01-27 22:46:10 -0800

    implement reset values button

--- app/src/main/java/com/example/course_grade_calculator/MainActivity.java
+++ app/src/main/java/com/example/course_grade_calculator/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements TextWatcher {
+public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnClickListener {
 
     EditText assignmentsEditText;
     EditText participationEditText;
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
     TextView examSeekBarTextView;
 
     EditText finalMarkEditText;
+    Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +53,9 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
         examSeekBarTextView = findViewById(R.id.examSeekBarTextView);
 
         finalMarkEditText = findViewById(R.id.finalMarkEditText);
+
+        resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(this);
     }
 
     private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
@@ -120,5 +126,21 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
     private void updateStandard() {
         double finalMark = assignmentsGrade * 15 / 100 + participationGrade * 15 / 100 + projectGrade * 20 / 100 + quizzesGrade * 20 / 100 + examSeekBarValue * 30 / 100;
         finalMarkEditText.setText(String.format("%.02f", finalMark));
+    }
+
+    @Override
+    public void onClick(View v) {
+        assignmentsGrade = 0.0;
+        assignmentsEditText.setText("0");
+        participationGrade = 0.0;
+        participationEditText.setText("0");
+        projectGrade = 0.0;
+        projectEditText.setText("0");
+        quizzesGrade = 0.0;
+        quizzesEditText.setText("0");
+        examSeekBarValue = 0.0;
+        examSeekBarTextView.setText("0");
+        examSeekBar.setProgress(0);
+        updateStandard();
     }
 }
